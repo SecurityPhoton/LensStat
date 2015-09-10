f609f87c2c096980b95589d60122a534a3137cea
Pontarr <pontarr@gmail.com>
2015-09-10 15:25:15 +0300

    initial project version

---
+++ Lenss/Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace Lenss
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(236, 12);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.IsValueShownAsLabel = true;
+            series1.Legend = "Legend1";
+            series1.Name = "Focal";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(340, 300);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 39);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Browse catalog";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(97, 62);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(97, 88);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(97, 114);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Total items";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Avr. focal lenth";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 117);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Avr. aperture";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 289);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(218, 23);
+            this.progressBar1.TabIndex = 4;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 321);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(929, 22);
+            this.statusStrip1.TabIndex = 5;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(132, 17);
+            this.toolStripStatusLabel1.Text = "Victor Bidnenko 2015 ©";
+            // 
+            // chart2
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart2.Legends.Add(legend2);
+            this.chart2.Location = new System.Drawing.Point(582, 12);
+            this.chart2.Name = "chart2";
+            this.chart2.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Fire;
+            series2.ChartArea = "ChartArea1";
+            series2.IsValueShownAsLabel = true;
+            series2.Legend = "Legend1";
+            series2.Name = "Aperture";
+            this.chart2.Series.Add(series2);
+            this.chart2.Size = new System.Drawing.Size(335, 300);
+            this.chart2.TabIndex = 6;
+            this.chart2.Text = "chart2";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(12, 152);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox4.Size = new System.Drawing.Size(218, 131);
+            this.textBox4.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(929, 343);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Lens Stat";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ Lenss/Form1.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ExifLib;
+using System.IO;
+
+namespace Lenss
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public class MyGraphNum
+        {
+            public int One { get; set; }
+            public int Two { get; set; }
+            public int Three { get; set; }
+            public int Four { get; set; }
+            public int Five { get; set; }
+            public int Six { get; set; }
+
+            public MyGraphNum()
+            { One = 0; Two = 0; Three = 0; Four = 0; Five = 0; Six = 0; }
+        }
+
+        public struct Exif
+        {
+            public double Aperture;
+            public double Focal;
+            public string Lens;
+            public string Camera;
+        }
+
+        public class Camera
+        {
+            public string CameraID { get; set; }
+            public int CamCount { get; set; }
+            public Camera() { }
+
+        }
+
+        public class Lens
+        {
+            public string LensID { get; set; }
+            public int LensCount { get; set; }
+            public Lens(){}
+        }
+
+        public void CamereLensId(List<Camera> L1,List<Lens>L2,Exif check)
+        {
+            
+            int index = L1.FindIndex(item => item.CameraID == check.Camera);
+            if (index == 0)
+            {
+                // element not exists, do what you need
+                L1.Add(new Camera { CameraID = check.Camera, CamCount = 1 });
+            }
+            else
+            {
+                foreach (var cc in L1.Where(x => x.CameraID == check.Camera))
+                    cc.CamCount++;
+            }
+
+            int index2 = L2.FindIndex(item => item.LensID == check.Lens);
+            if (index2 == 0)
+            {
+                // element not exists, do what you need
+                L2.Add(new Lens { LensID = check.Lens, LensCount = 1 });
+            }
+            else
+            {
+                foreach (var lc in L2.Where(x => x.LensID == check.Lens))
+                    lc.LensCount++;
+            }
+        }
+
+
+        public Exif RFocus(string name)
+        {
+            Exif exif;
+            try
+            {
+                using (ExifReader reader = new ExifReader(name))
+                {
+
+                    reader.GetTagValue<double>(ExifTags.FocalLength, out exif.Focal); if (exif.Focal > 250) { exif.Focal = 250; }
+                    reader.GetTagValue<double>(ExifTags.ApertureValue, out exif.Aperture); if (exif.Aperture > 32) { exif.Aperture = 32; }
+                    reader.GetTagValue<string>(ExifTags.LensModel, out exif.Lens);
+                    reader.GetTagValue<string>(ExifTags.Model, out exif.Camera);
+
+                    return exif;
+                }
+            }
+            catch (Exception ex)
+            {
+                exif.Aperture = 0; exif.Camera = ""; exif.Focal = 0; exif.Lens = ""; return exif;
+            }
+        }
+
+
+        public void DoWork()
+        {
+            var  ListCam = new List<Camera>();
+            var ListLens = new List<Lens>() ;
+
+            progressBar1.Minimum = 0;
+
+            this.chart1.Series["Focal"].Points.Clear();
+            this.chart2.Series["Aperture"].Points.Clear();
+            chart1.ChartAreas["ChartArea1"].AxisX.MajorGrid.LineWidth = 0;
+            chart1.ChartAreas["ChartArea1"].AxisY.MajorGrid.LineWidth = 0;
+
+            chart2.ChartAreas["ChartArea1"].AxisX.MajorGrid.LineWidth = 0;
+            chart2.ChartAreas["ChartArea1"].AxisY.MajorGrid.LineWidth = 0;
+            MyGraphNum init = new MyGraphNum();
+            MyGraphNum init2 = new MyGraphNum();
+
+            folderBrowserDialog1.ShowDialog();
+            var files = Directory.EnumerateFiles(folderBrowserDialog1.SelectedPath, "*.*", SearchOption.AllDirectories)
+            .Where(s => s.EndsWith(".jpg"));
+
+            progressBar1.Maximum = files.Count();
+            double sum = 0;
+            double sumA = 0;
+            double temp2=0;
+            Exif temp;
+            int temp1 = 0;
+            int truecountF = 0;
+            int truecountA = 0;
+            try
+            {
+                foreach (var item in files)
+                {
+                    progressBar1.Value++;
+                    temp = RFocus(item);
+                    CamereLensId(ListCam, ListLens, temp);
+                    if (!String.IsNullOrEmpty(Convert.ToString(temp.Focal)))
+                    {
+                        temp1 = Convert.ToInt32(Math.Round(temp.Focal, 0));
+                        if (temp1 < 20) { init.One++; }
+                        else if (temp1 < 31) { init.Two++; }
+                        else if (temp1 < 51) { init.Three++; }
+                        else if (temp1 < 86) { init.Four++; }
+                        else if (temp1 < 136) { init.Five++; }
+                        else if (temp1 > 135) { init.Six++; }
+
+                       sum += Convert.ToDouble(temp.Focal);
+                       truecountF++;
+
+                        if (!String.IsNullOrEmpty((temp.Aperture.ToString())))
+                        {
+                            temp2 = Math.Round(temp.Aperture, 2);
+                            if (temp2 < 2.0) { init2.One++; }
+                            else if (temp2 < 4.0) { init2.Two++; }
+                            else if (temp2 < 5.7) { init2.Three++; }
+                            else if (temp2 < 8.1) { init2.Four++; }
+                            else if (temp2 < 10.1) { init2.Five++; }
+                            else if (temp2 > 10) { init2.Six++; }
+                            sumA += Convert.ToDouble(temp.Aperture);
+                            truecountA++;
+                        }
+
+                    }
+
+                }
+                sum = Math.Round(sum / truecountF, 0);
+                sumA = Math.Round(sumA / truecountA, 1);
+                textBox1.Text = Convert.ToString(files.Count());
+                textBox2.Text = sum.ToString();
+                textBox3.Text = sumA.ToString();
+
+            }
+            catch (Exception ex)
+            {
+                // ex.ToString()
+                //textBox1.AppendText("Error!\n");
+            }
+
+
+            progressBar1.Value = 0;
+            this.chart1.Series["Focal"].Points.AddXY("0-20", init.One);
+            this.chart1.Series["Focal"].Points.AddXY("20-30", init.Two);
+            this.chart1.Series["Focal"].Points.AddXY("30-50", init.Three);
+            this.chart1.Series["Focal"].Points.AddXY("50-85", init.Four);
+            this.chart1.Series["Focal"].Points.AddXY("85-130", init.Five);
+            this.chart1.Series["Focal"].Points.AddXY("130-x", init.Six);
+
+            this.chart2.Series["Aperture"].Points.AddXY("<2.0", init2.One);
+            this.chart2.Series["Aperture"].Points.AddXY("<4", init2.Two);
+            this.chart2.Series["Aperture"].Points.AddXY("<5.6", init2.Three);
+            this.chart2.Series["Aperture"].Points.AddXY("<8", init2.Four);
+            this.chart2.Series["Aperture"].Points.AddXY("<10", init2.Five);
+            this.chart2.Series["Aperture"].Points.AddXY("10<x", init2.Six);
+
+            textBox4.AppendText("Cameras:\n");
+            foreach (var item in ListCam)
+            {
+                textBox4.AppendText(item.CameraID+" - "+item.CamCount+"\n");
+            }
+
+            textBox4.AppendText("Lenses:\n");
+            foreach (var item in ListLens)
+            {
+                textBox4.AppendText(item.LensID+" - "+item.LensCount+"\n");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DoWork();
+        }
+    }
+}
